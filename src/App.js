07ac56d009150448eca23acbab18bac9f8e0bcb3
Ahrnee <ahrndee@gmail.com>
2020-05-10 07:23:09 +0100

    updating tests

--- src/App.js
+++ src/App.js
@@ -23,10 +23,10 @@ const ALL_PEOPLE = gql`
 export default function App({ client }) {
   const [showIssueNotes, setShowIssueNotes] = useState(false);
   const { methods: { addCacheSnapshotToLog }, components: { SnapshotLogViewer } } = useCacheSnapshots({ client });
-  const { loading, data } = useQuery(ALL_PEOPLE);
-
+  const { loading, data: useQueryOriginatedData } = useQuery(ALL_PEOPLE);
+  const [watchQueryOriginatedData, setWatchQueryOriginatedData] = useState();
+  const nextNamePostfixNumber = useRef();
   const nextUserIdRef = useRef();
-  useEffect(() => { nextUserIdRef.current = 4; }, []);
 
   //
   const fetchPeople = (fetchPolicy = "cache-only") => {
@@ -47,43 +47,34 @@ export default function App({ client }) {
   }
 
   //
-  const updatePersonName = ({ id, name }) => {
-    console.log('writeFragment');
+  const updatePersonNameViaClient = ({ id, name }) => {
+    console.log('updatePersonNameViaClient', { id, name });
     client.writeFragment({ fragment: PERSON_FRAGMENT, id: defaultDataIdFromObject({ __typename: 'Person', id }), data: { name } });
-  }
-
-  const updatePersonNameSilently = ({ id, name }) => {
-    console.log('writeFragment');
-    client.cache.writeFragment({ fragment: PERSON_FRAGMENT, id: defaultDataIdFromObject({ __typename: 'Person', id }), data: { name } });
+    nextNamePostfixNumber.current++;
   }
 
   //
-  const createNewPersonWithMissingFieldQuery = () => {
-    fetchPeople().then((people) => {
-      const newData = { people: [...people, { __typename: 'Person', id: `${nextUserIdRef.current}`, name: `New Person ${nextUserIdRef.current}` }] };
-      nextUserIdRef.current += 1;
-      console.log('writeQuery. newData', newData);
-      client.writeQuery({ query: ALL_PEOPLE, data: newData, });
-      addCacheSnapshotToLog(`createNewPersonWithMissingFieldQuery`);
-    })
+  const updatePersonNameViaCache = ({ id, name }) => {
+    console.log('updatePersonNameViaCache', { id, name });
+    client.cache.writeFragment({ fragment: PERSON_FRAGMENT, id: defaultDataIdFromObject({ __typename: 'Person', id }), data: { name } });
+    nextNamePostfixNumber.current++;
   }
 
-  const startQueryWatch = (query) => {
-    client.cache.watch({
-      query,
-      optimistic: false,
-      callback: (data) => {
-        console.log('CACHE WATCH QUERY CALLBACK - ALL_PEOPLE', data);
-      },
+  //
+  const initQueryWatch = () => {
+    const watchedQuery = client.watchQuery({ query: ALL_PEOPLE });
+    watchedQuery.subscribe(next => {
+      setWatchQueryOriginatedData(next?.data);
+      console.log(`CLIENT WATCH QUERY CALLBACK - ALL_PEOPLE`, next)
     });
-
-    const watchedQuery = client.watchQuery({
-      query,
-    })
-    watchedQuery.subscribe(next => console.log(`CLIENT WATCH QUERY CALLBACK - ALL_PEOPLE`, next, watchedQuery));
-
     console.log('Started Query Watch for Query', ALL_PEOPLE);
   }
+
+  useEffect(() => {
+    initQueryWatch();
+    nextNamePostfixNumber.current = 1;
+    nextUserIdRef.current = 4;
+  }, [])
 
   return (
     <main>
@@ -105,31 +96,36 @@ export default function App({ client }) {
       <hr />
       <h2>Demo</h2>
 
-      <h3>Users</h3>
-      {!data || !data.people ? (<p>No People Loaded</p>) : (
+      <h3>Users (useQuery originated data)</h3>
+      {!useQueryOriginatedData || !useQueryOriginatedData.people ? (<p>No People Loaded</p>) : (
         <ul>
-          {data.people.map(personItem => (
+          {useQueryOriginatedData.people.map(personItem => (
             <li key={personItem.id}>
-              {personItem.name}
-              <button onClick={() => updatePersonName({ id: personItem.id, name: personItem.name + '-' })}>Person Name Update</button>
-              <button onClick={() => updatePersonNameSilently({ id: personItem.id, name: personItem.name + '-' })}>Person Name Silent Update</button>
+              {personItem.name}:
+              <button onClick={() => updatePersonNameViaClient({ id: personItem.id, name: personItem.name + ` ${nextNamePostfixNumber.current}` })}>Update name via ApolloClient</button>
+              <button onClick={() => updatePersonNameViaCache({ id: personItem.id, name: personItem.name + ` ${nextNamePostfixNumber.current}` })}>Update name via ApolloCache</button>
             </li>
           ))}
         </ul>
       )}
 
-      <SnapshotLogViewer />
+      <h3>Users (watchQuery originated data)</h3>
+      {!watchQueryOriginatedData || !watchQueryOriginatedData.people ? (<p>No People Loaded</p>) : (
+        <ul>
+          {watchQueryOriginatedData.people.map(personItem => (
+            <li key={personItem.id}>
+              {personItem.name}:
+              <button onClick={() => updatePersonNameViaClient({ id: personItem.id, name: personItem.name + ` ${nextNamePostfixNumber.current}` })}>Update name via ApolloClient</button>
+              <button onClick={() => updatePersonNameViaCache({ id: personItem.id, name: personItem.name + ` ${nextNamePostfixNumber.current}` })}>Update name via ApolloCache</button>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <h3>Actions</h3>
       <div>
         <div style={{ padding: 5 }}>
           <button onClick={() => createNewPersonQuery()}>Create New Person (all fields)</button>
-        </div>
-        <div style={{ padding: 5 }}>
-          <button onClick={() => createNewPersonWithMissingFieldQuery()}>Create New Person (missing field)</button>
-        </div>
-        <div style={{ padding: 5 }}>
-          <button onClick={() => startQueryWatch(ALL_PEOPLE)}>Start Query Watch - ALL_PEOPLE</button>
         </div>
       </div>
     </main>
